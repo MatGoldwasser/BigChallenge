754fbc2fd95835d4f115e245074ee4cc1b6fd744
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-09-28 20:47:03 -0300

    feat: submission model

---
+++ app/Http/Controllers/Submissions/SubmissionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Submissions;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SubmissionRequest;
+use Illuminate\Http\Request;
+
+class SubmissionController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(SubmissionRequest $request)
+    {
+        //
+    }
+}
